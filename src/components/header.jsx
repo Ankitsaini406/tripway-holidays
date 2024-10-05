db7b8fe4a737aeb1415c561573c30656d93e5792
Ankit Saini <as.ankitsaini406@gmail.com>
2024-10-05 14:28:02 +0530

    "navbar responsive"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
-import '../styles/header.css'
+import React, { useState } from 'react';
+import '../styles/header.css';
 
 function Header() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className='navbar'>
             <div className='header'>
                 <div className='logo'>
-                    <h1>TripWay Holidays</h1>
+                    <h1>TripWay&nbsp;Holidays</h1>
                 </div>
-                <div className='header-items'>
-                <ul className='header-list'>
-                    <li>Home</li>
-                    <li>Trips</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                    <button>Login</button>
-                </ul>
+
+                {
+                    isMenuOpen ? <div className="hamburger" onClick={toggleMenu}>
+                        X
+                    </div> : <div className="hamburger" onClick={toggleMenu}>
+                        ☰
+                    </div>
+                }
+
+                <div className={`header-items ${isMenuOpen ? 'open' : ''}`}>
+                    <ul className='header-list'>
+                        <li>Home</li>
+                        <li>Trips</li>
+                        <li>About</li>
+                        <li>Contact Us</li>
+                        <button className='header-button'>Login</button>
+                    </ul>
                 </div>
+
+                {/* Overlay */}
+                {isMenuOpen && <div className='overlay' onClick={toggleMenu}></div>}
+
             </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
