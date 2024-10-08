97afe0e78eb4d1291601a41d6afc51813e58a207
Ankit Saini <as.ankitsaini406@gmail.com>
2024-10-08 17:28:35 +0530

    "advanceserchbar done"

--- src/components/advanceserch.jsx
+++ src/components/advanceserch.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import { FaSearch } from "react-icons/fa";
 import { PiCarProfileLight } from "react-icons/pi";
 import { MdCardTravel } from "react-icons/md";
 import { HiOutlineUserGroup } from "react-icons/hi2";
+import "react-datepicker/dist/react-datepicker.css";
 import "../styles/components/advanceserchbar.css";
 
 function AdvancedSearchBar() {
@@ -15,6 +15,8 @@ function AdvancedSearchBar() {
     const [startDate, setStartDate] = useState(null);
     const [passanger, setPassanger] = useState('Passanger');
     const [carOption, setCarOption] = useState("");
+    const [tourOption, setTourOption] = useState("");
+    const [groupOption, setGroupOption] = useState("");
     const [activeLink, setActiveLink] = useState("cabs");
     const [selectedRedio, setSelectedRedio] = useState("one-way");
     const [time, setTime] = useState("");
@@ -34,6 +36,21 @@ function AdvancedSearchBar() {
         { value: "hatech-back", label: "Hatech Back" },
     ];
 
+    const tourOptions = [
+        { value: "", label: "Select Tour"},
+        { value: "adventure", label: "Adventure"},
+        { value: "wildlife", label: "Wildlife"},
+        { value: "family-package", label: "Family Package"},
+        { value: "honeymoon", label: "Honeymoon Package"}
+    ]
+
+    const groupOptions = [
+        { value: "", label: "Select Tour"},
+        { value: "adventure", label: "Adventure"},
+        { value: "wildlife", label: "Wildlife"},
+        { value: "other", label: "Other"}
+    ]
+
     const handleClick = (e, link) => {
         e.preventDefault();
         setActiveLink(link);
@@ -42,6 +59,14 @@ function AdvancedSearchBar() {
     const handleSelectChange = (e) => {
         setCarOption(e.target.value);
     };
+
+    const handleTourChanges = (e) => {
+        setTourOption(e.target.value);
+    }
+
+    const handleGroupChanges = (e) => {
+        setGroupOption(e.target.value);
+    }
 
     const handleOneWaySearch = () => {
         console.log({
@@ -63,6 +88,18 @@ function AdvancedSearchBar() {
             time,
             carOption,
             passanger,
+        })
+    }
+
+    const handleTourPackage = () => {
+        console.log({
+            tourOption,
+        })
+    }
+
+    const handleGroupPackage = () => {
+        console.log({
+            groupOption,
         })
     }
 
@@ -112,109 +149,143 @@ function AdvancedSearchBar() {
             </ul>
 
             <div className="advanced-search-bar">
-                <div className="radio-option">
-                    <div>
-                        <input
-                            type="radio"
-                            value="one-way"
-                            checked={selectedRedio === "one-way"}
-                            onChange={handleOptionChange}
-                        />
-                        <label>One&nbsp;Way</label>
+                {activeLink === "cabs" && (<>
+                    <div className="radio-option">
+                        <div>
+                            <input
+                                type="radio"
+                                value="one-way"
+                                checked={selectedRedio === "one-way"}
+                                onChange={handleOptionChange}
+                            />
+                            <label>One&nbsp;Way</label>
+                        </div>
+                        <div>
+                            <input
+                                type="radio"
+                                value="round-trip"
+                                checked={selectedRedio === "round-trip"}
+                                onChange={handleOptionChange}
+                            />
+                            <label>Round&nbsp;Trip</label>
+                        </div>
                     </div>
-                    <div>
-                        <input
-                            type="radio"
-                            value="round-trip"
-                            checked={selectedRedio === "round-trip"}
-                            onChange={handleOptionChange}
-                        />
-                        <label>Round&nbsp;Trip</label>
-                    </div>
-                </div>
 
-                <div className="radio-option">
-                    <input
+                    <div className="radio-option">
+                        <input
+                            type="text"
+                            placeholder="From"
+                            value={fromTerm}
+                            onChange={(e) => setFromTerm(e.target.value)}
+                            className={`search-input`}
+                        />
+                        {selectedRedio === "round-trip" ? <></> : <input
+                            type="text"
+                            placeholder="To"
+                            value={toTerm}
+                            onChange={(e) => setToTerm(e.target.value)}
+                            className="search-input"
+                        />}
+                    </div>
+                    {selectedRedio === "round-trip" ? <div className="radio-option"><input
                         type="text"
-                        placeholder="From"
-                        value={fromTerm}
-                        onChange={(e) => setFromTerm(e.target.value)}
+                        placeholder="Destination"
+                        value={destination}
+                        onChange={(e) => setDestination(e.target.value)}
                         className={`search-input`}
-                    />
-                    {selectedRedio === "round-trip" ? <></> : <input
-                        type="text"
-                        placeholder="To"
-                        value={toTerm}
-                        onChange={(e) => setToTerm(e.target.value)}
-                        className="search-input"
-                    />}
-                </div>
-                {selectedRedio === "round-trip" ? <div className="radio-option"><input
-                    type="text"
-                    placeholder="Destination"
-                    value={destination}
-                    onChange={(e) => setDestination(e.target.value)}
-                    className={`search-input`}
-                /></div> : <></>}
-                <div className="radio-option">
-                    <DatePicker
-                        selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        selectsStart
-                        startDate={startDate}
-                        placeholderText="Start Date"
-                        className="date-picker"
-                    />
+                    /></div> : <></>}
+                    <div className="radio-option">
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            selectsStart
+                            startDate={startDate}
+                            placeholderText="Start Date"
+                            className="date-picker"
+                        />
 
-                    <input
-                        className="time-picker"
-                        type="time"
-                        value={time}
-                        onChange={handleTimeChange}
-                    />
-                </div>
+                        <input
+                            className="time-picker"
+                            type="time"
+                            value={time}
+                            onChange={handleTimeChange}
+                        />
+                    </div>
 
-                <div className="radio-option">
+                    <div className="radio-option">
+                        <select
+                            value={carOption}
+                            onChange={handleSelectChange}
+                            className="select-filter"
+                        >
+                            {options.map((option) => (
+                                <option key={option.value} value={option.value}>
+                                    {option.label}
+                                </option>
+                            ))}
+                        </select>
+
+                        <input
+                            type="number"
+                            placeholder="Passanger"
+                            value={passanger}
+                            min="1"
+                            max="7"
+                            onChange={(e) => setPassanger(e.target.value)}
+                            className="search-input"
+                        />
+                    </div>
+
+                    {
+                        selectedRedio === "round-trip" ? <></> : <input
+                            type="text"
+                            placeholder="Offer From"
+                            value={offerFrom}
+                            onChange={(e) => setOfferFrom(e.target.value)}
+                            className="search-input"
+                        />
+                    }
+
+                    {
+                        selectedRedio === "round-trip" ? <button className="search-button" onClick={handleTwoWaySearch}>
+                            <FaSearch />&nbsp;Search
+                        </button> : <button className="search-button" onClick={handleOneWaySearch}>
+                            <FaSearch />&nbsp;Search
+                        </button>
+                    }
+                </>)}
+
+                {activeLink === "tour-packages" && (<>
                     <select
-                        value={setCarOption}
-                        onChange={handleSelectChange}
+                        value={tourOption}
+                        onChange={handleTourChanges}
                         className="select-filter"
                     >
-                        {options.map((option) => (
+                        {tourOptions.map((option) => (
                             <option key={option.value} value={option.value}>
                                 {option.label}
                             </option>
                         ))}
                     </select>
+                    <button className="search-button" onClick={handleTourPackage}>
+                        <FaSearch />&nbsp;Search
+                    </button></>)}
 
-                    <input
-                        type="number"
-                        placeholder="Passanger"
-                        value={passanger}
-                        min="1"
-                        max="7"
-                        onChange={(e) => setPassanger(e.target.value)}
-                        className="search-input"
-                    />
-                </div>
-
-                {
-                    selectedRedio === "round-trip" ? <></> : <input
-                        type="text"
-                        placeholder="Offer From"
-                        value={offerFrom}
-                        onChange={(e) => setOfferFrom(e.target.value)}
-                        className="search-input"
-                    />
-                }
-
-                {
-                    selectedRedio === "round-trip" ? <button className="search-button" onClick={handleTwoWaySearch}>
-                        <FaSearch />
-                    </button> : <button className="search-button" onClick={handleOneWaySearch}>
-                        <FaSearch />
-                    </button>
-                }
+                {activeLink === "group-tour" && (<>
+                    <select
+                        value={groupOption}
+                        onChange={handleGroupChanges}
+                        className="select-filter"
+                    >
+                        {groupOptions.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
+                    <button className="search-button" onClick={handleGroupPackage}>
+                        <FaSearch />&nbsp;Search
+                    </button></>)}
             </div>
         </div>
         // </div>
