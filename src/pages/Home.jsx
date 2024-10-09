edc3aefafaddf3f1096969eb9a7b0531fcadef87
Ankit Saini <as.ankitsaini406@gmail.com>
2024-10-09 18:02:31 +0530

    "changes in slider"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,140 @@
-import React from "react";
-import '../styles/pages/home.css'
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import "../styles/pages/home.css";
 import Hero from "../components/hero";
-
+ 
 function Home() {
+    const [imageIndex, setImageIndex] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState(true);
+
+    const imageUrls = [
+        {
+            url: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 1",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 2",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?q=80&w=2548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 3",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1686127628300-1824272ab27a?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 4",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1595433306946-233f47e4af3a?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 5",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1609953886408-70f5f595b2fe?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 6",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1694608217345-75987f889c89?q=80&w=2552&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 7",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1592156386005-ad841f96224a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 8",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1669915746165-232236839501?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 9",
+        },
+        {
+            url: "https://images.unsplash.com/photo-1591122447574-933363c1dc5a?q=80&w=2643&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Nature 10",
+        },
+    ];
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+      };
+
+    const showNextImg = () => {
+        setImageIndex((prevIndex) => {
+            if (prevIndex === imageUrls.length - 1) {
+                return 0;
+            }
+            return prevIndex + 1;
+        });
+    };
+
+    const showPrevImg = () => {
+        setImageIndex((prevIndex) => {
+            if (prevIndex === 0) {
+                return imageUrls.length - 1;
+            }
+            return prevIndex - 1;
+        });
+    };
+
+    // Auto-play feature (with cleanup)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsTransitioning(true);
+            showNextImg();
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="home">
             <Hero />
+            <div className="tour-image-slider">
+                <div className="tour-slider">
+                    <div className="tour-slider-flex">
+                        {imageUrls.map(img => (
+                            <img src={img.url} alt={img.alt} className="img-slider-img"
+                                style={{
+                                    translate: `${-100 * imageIndex}%`,
+                                    transition: isTransitioning ? "translate 0.5s ease-in-out" : "none",
+                                }}
+                            />
+                        ))}
+                    </div>
+                    <button onClick={() => {
+                        setIsTransitioning(true);
+                        showPrevImg();
+                    }} className="img-slider-btn" style={{ left: 0 }}>&#10094;</button>
+                    <button onClick={() => {
+                        setIsTransitioning(true);
+                        showNextImg();
+                    }} className="img-slider-btn" style={{ right: 0 }}>&#10095;</button>
+                </div>
+            </div>
+            <Slider {...settings}>
+            <div>
+          <h3>1</h3>
         </div>
-    )
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+            </Slider>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
+
