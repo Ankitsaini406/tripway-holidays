d59f071a7550ef9ac52f313eb6ac4fc58d1770c4
Ankit Saini <as.ankitsaini406@gmail.com>
2024-10-12 12:32:39 +0530

    "footer"

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import '../styles/pages/footer.css'
 
-function Footer () {
-    return(
+function Footer() {
+    return (
         <div className="main-footer">
-            Footer
+            <div className="footer">
+                <div className="footer-flex">
+                    <div style={{ width: '8rem' }}>Quick Links</div>
+                    <ul className="footer-flex-list">
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div className="footer-flex">
+                    <div style={{ width: '8rem' }}>
+                        Programs
+                    </div>
+                    <ul className="footer-flex-list">
+                        <li>Cabs</li>
+                        <li>Tour Packges</li>
+                        <li>Group Tour</li>
+                    </ul>
+                </div>
+                <div className="footer-flex">
+                    <div style={{ width: '8rem' }}>
+                        Get In Touch
+                    </div>
+                    <div className="footer-flex-list">
+                        Near Chandpool gate, Sikar, Rajsthan
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
