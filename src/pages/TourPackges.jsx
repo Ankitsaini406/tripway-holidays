3168f9824a1017c8f016f64a3852e40003ca88f4
Ankit Saini <as.ankitsaini406@gmail.com>
2024-10-16 16:02:38 +0530

    "add tourpackges and changes style"

--- src/pages/TourPackges.jsx
+++ src/pages/TourPackges.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import "../styles/pages/layout.css";
 import '../styles/pages/tourpackges.css';
+import TourData from "../lib/tour-data";
 
 function TourPackes () {
     return (
         <div className="layout">
             <div className="tour">
                 <div className="filters">
-                    <h4>This is filter</h4>
+                    <Filters />
                 </div>
                 <div className="tour-box">
-                    <h4>This is tour box</h4>
+                    <TourCard />
                 </div>
             </div>
         </div>
+    )
+}
+
+export function Filters () {
+    return (
+        <div className="filters-box">
+            <h4>This is filter</h4>
+        </div>
+    )
+}
+
+export function TourCard () {
+    return (
+        <>
+        {
+            TourData.map((value) => (
+                <div key={value.id} className="tour-card">
+                    <img className="tour-image" src={value.img} alt="" srcset="" />
+                    <div className="tour-detalis">
+                        <h3>{value.title}</h3>
+                        <h6>{value.category}</h6>
+                        <p>{value.desc}</p>
+                        </div>
+                </div>
+            ))
+        }
+        </>
     )
 }
 
