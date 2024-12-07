73ca449fbbbbea8b6f721785a07e47484d762a12
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-07 11:40:39 +0530

    "why book icons"

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,11 @@ import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 import Hero from "@/components/hero";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { PiTrainLight } from "react-icons/pi";
+import { IoPricetagOutline } from "react-icons/io5";
+import { BiSolidOffer, BiSupport } from "react-icons/bi";
 import PopUp from "@/utils/popUp";
+import styles from "./page.module.css";
 
 const Home = () => {
     const imageRefs = useRef([]);
@@ -58,18 +61,22 @@ const Home = () => {
                     <h1>Why book with us?</h1>
                     <div className={styles.whyBook}>
                         <div className={styles.whyBox}>
+                            <div className={styles.icons}><PiTrainLight /></div>
                             <h2>Easy Booking</h2>
                             <p className={styles.whyText}>We offer easy and convenient bus booking with attractive offers.</p>
                         </div>
                         <div className={styles.whyBox}>
+                            <div className={styles.icons}><IoPricetagOutline /></div>
                             <h2>Lowest Price</h2>
                             <p className={styles.whyText}>We ensure low rates on hotel reservation, holiday packages and on bus tickets.</p>
                         </div>
                         <div className={styles.whyBox}>
+                            <div className={styles.icons}><BiSolidOffer /></div>
                             <h2>Exciting Deals</h2>
                             <p className={styles.whyText}>Enjoy exciting deals on buses, hotel, car rental and tour packages.</p>
                         </div>
                         <div className={styles.whyBox}>
+                            <div className={styles.icons}><BiSupport /></div>
                             <h2>24/7 Support</h2>
                             <p className={styles.whyText}>Get assistance 24/7 on any kind of travel related query. We are happy to assist you.</p>
                         </div>
