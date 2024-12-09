fe7ac8d5b1528add433fa0133c5385753610afb4
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-09 17:49:28 +0530

    "add robots text"

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,21 @@ const nextConfig = {
         BOT_URL: process.env.NEXT_APP_BOT_URL,
         GA_TRACKING_ID: process.env.NEXT_APP_GA_TRACKING_ID,
     },
+
+    async redirects() {
+        return [
+            {
+                source: "/robots.txt",
+                destination: "/api/robots",
+                permanent: true,
+            },
+        ];
+    },
+    // webpack(config) {
+    //     // Example Webpack customizations
+    //     config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    //     return config;
+    // },
 };
 
 export default nextConfig;
---
+++ src/app/api/robots/route.js
@@ -0,0 +1,20 @@
+
+export async function GET() {
+
+    const localApi = process.env.API_URL;
+    const productionApi = process.env.HOST_URL;
+    const apiPoint = process.env.NODE_ENV === "development" ? localApi : productionApi;
+
+    const robots = `
+        User-agent: *
+        Allow: /
+        Disallow: []
+        Sitemap: ${apiPoint}/sitemap.xml
+    `;
+
+    return new Response(robots.trim(), {
+        headers: {
+            "Content-Type": "text/plain",
+        },
+    });
+}
