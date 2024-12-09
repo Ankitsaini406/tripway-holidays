14766303509c672ca4854085bae3a9e7c183994b
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-09 16:45:15 +0530

    "change for seo"

--- src/app/childLayout.js
+++ src/app/childLayout.js
@@ -1,22 +1,16 @@
 'use client';
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import Head from "next/head";
 import { usePathname } from "next/navigation";
 import { ToastContainer } from "react-toastify";
 import GoogleAnalytics from "./googleAnalytics";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
-import { routeMetaData } from "@/types/metadataConfig";
 
 export default function ClientRootLayout({ children }) {
     const pathname = usePathname();
-    const [metadata, setMetadata] = useState({
-        title: "TripWay Holidays",
-        description: "Book Tour Next Tripway Holidays",
-    });
 
     const noHeaderFooterRoutes = ['/auth/client-login', '/auth/signup', '/auth/agent-login'];
     const hideHeaderFooter = noHeaderFooterRoutes.includes(pathname);
@@ -38,23 +32,10 @@ export default function ClientRootLayout({ children }) {
                 document.body.removeChild(script);
             };
         }
-
-        const meta = routeMetaData[pathname] || {
-            title: "TripWay Holidays",
-            description: "Book Tour Next Tripway Holidays",
-        };
-        setMetadata(meta);
     }, [hideHeaderFooter, pathname, bot_url]);
 
     return (
         <>
-            <Head>
-                <title>{metadata.title}</title>
-                <meta name="description" content={metadata.description} />
-                <meta property="og:title" content={metadata.title} />
-                <meta property="og:description" content={metadata.description} />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
             <GoogleAnalytics />
             {!hideHeaderFooter && <Header />}
             <main>{children}</main>
--- src/app/page.js
+++ src/app/page.js
@@ -21,10 +21,73 @@ const Home = () => {
             <div className='layout'>
                 <Hero />
 
+                <div className={styles.homeTour} id="oneWay">
+                    <div className={styles.homeTourFlex}>
+                        <div>
+                            <h1>One Way</h1>
+                            <p>At Tripway Holidays, we craft group tours that blend exploration, spirituality, and cultural discovery, creating unforgettable experiences for every traveler. Imagine embarking on a spiritual journey that inspires inner peace, where harmonious clusters of like-minded travelers share moments of reflection and connection. Our heritage tours delve into the heart of traditions, art, and history, offering a rich tapestry of cultural wonders. With our Freedom Tours, we provide the perfect mix of independence and camaraderie, allowing you to explore destinations with flexibility while enjoying the company of a supportive group. For those seeking excitement, our Special Event Tours celebrate vibrant festivals, grand concerts, and unique gatherings, turning every moment into a cherished memory. At Tripway Holidays, we ensure every journey is a balance of discovery, connection, and joy, designed to resonate with your spirit.</p>
+                        </div>
+                        <div className={styles.imgBox}>
+                            <Image
+                                className={styles.tourImg}
+                                data-src='/slider/slider6.png'
+                                src='/slider/slider6.png'
+                                alt="multicolored-buntings"
+                                placeholder="blur"
+                                blurDataURL='/slider/slider6.png'
+                                width={1600}
+                                height={900}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.homeTour} id="roundTrip">
+                    <div className={styles.homeTourFlex}>
+                        <div>
+                            <h1>Round Trip</h1>
+                            <p>At Tripway Holidays, we craft group tours that blend exploration, spirituality, and cultural discovery, creating unforgettable experiences for every traveler. Imagine embarking on a spiritual journey that inspires inner peace, where harmonious clusters of like-minded travelers share moments of reflection and connection. Our heritage tours delve into the heart of traditions, art, and history, offering a rich tapestry of cultural wonders. With our Freedom Tours, we provide the perfect mix of independence and camaraderie, allowing you to explore destinations with flexibility while enjoying the company of a supportive group. For those seeking excitement, our Special Event Tours celebrate vibrant festivals, grand concerts, and unique gatherings, turning every moment into a cherished memory. At Tripway Holidays, we ensure every journey is a balance of discovery, connection, and joy, designed to resonate with your spirit.</p>
+                        </div>
+                        <div className={styles.imgBox}>
+                            <Image
+                                className={styles.tourImg}
+                                data-src='/slider/slider6.png'
+                                src='/slider/slider6.png'
+                                alt="multicolored-buntings"
+                                placeholder="blur"
+                                blurDataURL='/slider/slider6.png'
+                                width={1600}
+                                height={900}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.homeTour} id="multiCity">
+                    <div className={styles.homeTourFlex}>
+                        <div>
+                            <h1>Multi City Trip</h1>
+                            <p>At Tripway Holidays, we craft group tours that blend exploration, spirituality, and cultural discovery, creating unforgettable experiences for every traveler. Imagine embarking on a spiritual journey that inspires inner peace, where harmonious clusters of like-minded travelers share moments of reflection and connection. Our heritage tours delve into the heart of traditions, art, and history, offering a rich tapestry of cultural wonders. With our Freedom Tours, we provide the perfect mix of independence and camaraderie, allowing you to explore destinations with flexibility while enjoying the company of a supportive group. For those seeking excitement, our Special Event Tours celebrate vibrant festivals, grand concerts, and unique gatherings, turning every moment into a cherished memory. At Tripway Holidays, we ensure every journey is a balance of discovery, connection, and joy, designed to resonate with your spirit.</p>
+                        </div>
+                        <div className={styles.imgBox}>
+                            <Image
+                                className={styles.tourImg}
+                                data-src='/slider/slider6.png'
+                                src='/slider/slider6.png'
+                                alt="multicolored-buntings"
+                                placeholder="blur"
+                                blurDataURL='/slider/slider6.png'
+                                width={1600}
+                                height={900}
+                            />
+                        </div>
+                    </div>
+                </div>
+
                 <div className={styles.homeTour} id="groupTour">
                     <div className={styles.homeTourFlex}>
                         <div>
-                            <h2>Group Tours</h2>
+                            <h1>Group Tours</h1>
                             <p>At Tripway Holidays, we craft group tours that blend exploration, spirituality, and cultural discovery, creating unforgettable experiences for every traveler. Imagine embarking on a spiritual journey that inspires inner peace, where harmonious clusters of like-minded travelers share moments of reflection and connection. Our heritage tours delve into the heart of traditions, art, and history, offering a rich tapestry of cultural wonders. With our Freedom Tours, we provide the perfect mix of independence and camaraderie, allowing you to explore destinations with flexibility while enjoying the company of a supportive group. For those seeking excitement, our Special Event Tours celebrate vibrant festivals, grand concerts, and unique gatherings, turning every moment into a cherished memory. At Tripway Holidays, we ensure every journey is a balance of discovery, connection, and joy, designed to resonate with your spirit.</p>
                             <Link className='readMore' href='/group-tour'>Read More</Link>
                         </div>
@@ -44,26 +107,26 @@ const Home = () => {
                 </div>
 
                 <div className={styles.mainBox}>
-                    <h1>Why book with us?</h1>
+                    <h2>Why book with Tripway Holidays ?</h2>
                     <div className={styles.whyBook}>
                         <div className={styles.whyBox}>
                             <div className={styles.icons}><PiTrainLight /></div>
-                            <h2>Easy Booking</h2>
+                            <h3>Easy Booking</h3>
                             <p className={styles.whyText}>We offer easy and convenient bus booking with attractive offers.</p>
                         </div>
                         <div className={styles.whyBox}>
                             <div className={styles.icons}><IoPricetagOutline /></div>
-                            <h2>Lowest Price</h2>
+                            <h3>Lowest Price</h3>
                             <p className={styles.whyText}>We ensure low rates on hotel reservation, holiday packages and on bus tickets.</p>
                         </div>
                         <div className={styles.whyBox}>
                             <div className={styles.icons}><BiSolidOffer /></div>
-                            <h2>Exciting Deals</h2>
+                            <h3>Exciting Deals</h3>
                             <p className={styles.whyText}>Enjoy exciting deals on buses, hotel, car rental and tour packages.</p>
                         </div>
                         <div className={styles.whyBox}>
                             <div className={styles.icons}><BiSupport /></div>
-                            <h2>24/7 Support</h2>
+                            <h3>24/7 Support</h3>
                             <p className={styles.whyText}>Get assistance 24/7 on any kind of travel related query. We are happy to assist you.</p>
                         </div>
                     </div>
