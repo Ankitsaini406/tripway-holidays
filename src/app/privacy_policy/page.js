aec7672f05eb5661470ce7feb66c5039a249f115
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-11 17:29:58 +0530

    "remove changes"

--- src/app/privacy_policy/page.js
+++ src/app/privacy_policy/page.js
@@ -129,7 +129,7 @@ const PrivacyPolicy = () => {
             <h4 className={styles.h4Tag}>Usage Data</h4>
             <p>Usage Data is collected automatically when using the Service.</p>
             <p>
-                Usage Data may include information such as Your Device's Internet
+                Usage Data may include information such as Your Device&apos;s Internet
                 Protocol address (e.g. IP address), browser type, browser version, the
                 pages of our Service that You visit, the time and date of Your visit,
                 the time spent on those pages, unique device identifiers and other
@@ -163,13 +163,13 @@ const PrivacyPolicy = () => {
             <p>
                 If You decide to register through or otherwise grant us access to a
                 Third-Party Social Media Service, We may collect Personal data that is
-                already associated with Your Third-Party Social Media Service's account,
+                already associated with Your Third-Party Social Media Service&apos;s account,
                 such as Your name, Your email address, Your activities or Your contact
                 list associated with that account.
             </p>
             <p>
                 You may also have the option of sharing additional information with the
-                Company through Your Third-Party Social Media Service's account. If You
+                Company through Your Third-Party Social Media Service&apos;s account. If You
                 choose to provide such information and Personal Data, during
                 registration or otherwise, You are giving the Company permission to use,
                 share, and store it in a manner consistent with this Privacy Policy.
@@ -280,7 +280,7 @@ const PrivacyPolicy = () => {
                 <li className={styles.liTag}>
                     <strong>To contact You:</strong> To contact You by email, telephone
                     calls, SMS, or other equivalent forms of electronic communication,
-                    such as a mobile application's push notifications regarding updates or
+                    such as a mobile application&apos;s push notifications regarding updates or
                     informative communications related to the functionalities, products or
                     contracted services, including the security updates, when necessary or
                     reasonable for their implementation.
@@ -373,7 +373,7 @@ const PrivacyPolicy = () => {
 
             <h3 className={styles.h3Tag}>Transfer of Your Personal Data</h3>
             <p>
-                Your information, including Personal Data, is processed at the Company's
+                Your information, including Personal Data, is processed at the Company&apos;s
                 operating offices and in any other places where the parties involved in
                 the processing are located. It means that this information may be
                 transferred to — and maintained on — computers located outside of Your
@@ -461,7 +461,7 @@ const PrivacyPolicy = () => {
                 security.
             </p>
 
-            <h2 className={styles.h2Tag}>Children's Privacy</h2>
+            <h2 className={styles.h2Tag}>Children&apos;s Privacy</h2>
             <p>
                 Our Service does not address anyone under the age of 13. We do not
                 knowingly collect personally identifiable information from anyone under
@@ -474,7 +474,7 @@ const PrivacyPolicy = () => {
             <p>
                 If We need to rely on consent as a legal basis for processing Your
                 information and Your country requires consent from a parent, We may
-                require Your parent's consent before We collect and use that
+                require Your parent&apos;s consent before We collect and use that
                 information.
             </p>
 
