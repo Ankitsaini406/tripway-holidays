c5e5a3fcf02c8a5d88a0d56cb6088d252a7000af
Ankit Saini <as.ankitsaini406@gmail.com>
2024-12-14 15:39:22 +0530

    "Footer update"

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,49 +1,81 @@
 import React from 'react';
 import styles from '@/styles/components/footer.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Footer() {
 
     return (
         <div className={styles.mainFooter}>
             <div className={styles.imageContainer}>
-                <Image 
+                <Image
                     src='/footer.webp'
                     alt='footer'
                     layout="fill"
                     objectFit="fill"
                 />
                 <div className={styles.textOverlay}>
-                <div className={styles.footer}>
-                <div className={styles.footerFlex}>
-                    <div style={{ width: '8rem' }}>Quick Links</div>
-                    <ul className={styles.footerFlexList}>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
+                    <div className={styles.footer}>
+                        <div className={styles.footerFlex}>
+                            <Image src='/favicon.ico'
+                                alt='Tripway Holidays'
+                                width={100}
+                                height={100}
+                            />
+                            <h3>Follow Us</h3>
+                            <div className={styles.scoialLinks}>
+                                <Link href="https://www.instagram.com/tripwayholiday" target="_blank">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+                                        alt="Instagram"
+                                        width={100}
+                                        height={100}
+                                    />
+                                </Link>
+                                <Link href="https://www.facebook.com/tripwayholidays" target="_blank">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png"
+                                        alt="Facebook"
+                                        width={100}
+                                        height={100}
+                                    />
+                                </Link>
+                                <Link href="https://www.youtube.com/@tripwayholidays" target="_blank">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png"
+                                        alt="You Tube"
+                                        width={100}
+                                        height={100}
+                                    />
+                                </Link>
+                                <Link href="https://x.com/tripwayholidays" target="_blank">
+                                    <Image src="https://upload.wikimedia.org/wikipedia/commons/b/b7/X_logo.jpg"
+                                        alt="Twitter"
+                                        width={100}
+                                        height={100}
+                                    />
+                                </Link>
+                            </div>
+                        </div>
 
-                <div className={styles.footerFlex}>
-                    <div style={{ width: '8rem' }}>
-                        Programs
-                    </div>
-                    <ul className={styles.footerFlexList}>
-                        <li>Cabs</li>
-                        <li>Tour Packages</li>
-                        <li>Group Tour</li>
-                    </ul>
-                </div>
+                        <div className={styles.footerFlex}>
+                            <h3>Useful Links</h3>
+                            <ul className={styles.footerFlexList}>
+                                <Link className={styles.lnkTag} href='/'>Home</Link>
+                                <Link className={styles.lnkTag} href='#cabs'>Cabs</Link>
+                                <Link className={styles.lnkTag} href='/group-tours'>Group Tours</Link>
+                                <Link className={styles.lnkTag} href='/auth/client-login'>Login</Link>
+                                <Link className={styles.lnkTag} href='/auth/signup'>Sign Up</Link>
+                            </ul>
+                        </div>
 
-                <div className={styles.footerFlex}>
-                    <div style={{ width: '8rem' }}>
-                        Get In Touch
+                        <div className={styles.footerFlex}>
+                            <h3>Condition</h3>
+                            <ul className={styles.footerFlexList}>
+                                <Link className={styles.lnkTag} href='/privacy_policy'>Pricay policy</Link>
+                                <Link className={styles.lnkTag} href='/terms_and_condition'>Trems & Condition</Link>
+                                <Link className={styles.lnkTag} href="/about" >About Us</Link>
+                                <Link className={styles.lnkTag} href='/contact_us'>Contact Us</Link>
+                            </ul>
+                        </div>
                     </div>
-                    <div className={styles.footerFlexList}>
-                        Near Chandpool gate, Sikar, Rajasthan
-                    </div>
-                </div>
-            </div>
                 </div>
             </div>
         </div>
