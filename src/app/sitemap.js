868405b119050367ae4b358f41f2c115156bc747
Ankit Saini <as.ankitsaini406@gmail.com>
2025-01-03 11:45:21 +0530

    "sitemap changes"

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,10 +1,9 @@
-
 export default async function sitemap() {
-
     const localApi = process.env.API_URL;
     const productionApi = process.env.HOST_URL;
     const apiPoint = process.env.NODE_ENV === "development" ? localApi : productionApi;
 
+    // Fetch dynamic tour data
     const response = await fetch(`${apiPoint}api/group-tours`);
     if (!response.ok) {
         throw new Error("Failed to fetch tour data");
@@ -12,18 +11,37 @@ export default async function sitemap() {
 
     const data = await response.json();
 
+    // Generate dynamic tour URLs
     const tourDetails = data?.map((tour) => {
         return {
             url: `${apiPoint}group-tours/${tour?.slug}`,
-            lastModified: tour?.createdAt,
+            lastModified: `${new Date(tour?.createdAt).toISOString()}`,
         };
     });
 
+    // Add static pages
+    const staticPages = [
+        {
+            url: `${apiPoint}/group-tour`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${apiPoint}/about`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${apiPoint}/contact_us`,
+            lastModified: new Date().toISOString(),
+        },
+    ];
+
+    // Combine static pages and dynamic tour URLs
     return [
         {
             url: `${apiPoint}`,
-            lastModified: new Date(),
+            lastModified: new Date().toISOString(),
         },
         ...tourDetails,
-    ]
+        ...staticPages,
+    ];
 }
