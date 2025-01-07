1da1a4da7ecbb6a856743112f0070d8e2de5dd01
Ankit Saini <as.ankitsaini406@gmail.com>
2025-01-07 16:58:39 +0530

    "changes and aad metadata in pages"

--- src/app/auth/client-login/page.js
+++ src/app/auth/client-login/page.js
@@ -1,144 +1,37 @@
-'use client';
+import ClientLoaginPage from "./clientLogin";
 
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import AgentLogin from "../agent-login/page";
-import ForgetPassword from "@/utils/forgetPassword";
-import { FaHome } from "react-icons/fa";
-import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
-import { useClient } from "@/context/UserContext";
-import styles from '@/styles/pages/authpage.module.css';
-import Link from "next/link";
+const localApi = process.env.API_URL;
+const productionApi = process.env.HOST_URL;
+const apiPoint = process.env.NODE_ENV === "development" ? localApi : productionApi;
 
-function ClientLoaginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [activeContainer, setActiveContainer] = useState(true);
-    const [agentLogin, setAgentLogin] = useState(false);
-    const [isHovered, setIsHovered] = useState(false);
-    const [showPassword, setShowPassword] = useState(false);
-    const { loginUser } = useClient();
-    const router = useRouter();
+export const metadata = {
+    title: "Login",
+    description: "Access your Tripway Holidays account. Log in to manage your bookings, explore exclusive holiday packages, and more.",
+    keywords: [
+        "Login Tripway Holidays",
+        "Access Tripway Account",
+        "Holiday Booking Login",
+        "Tripway Holidays Login Page",
+        "Sign In for Trip Packages",
+        "Manage Tripway Bookings",
+        "Tripway Holidays Login Form",
+    ],
+    openGraph: {
+        title: "Login",
+        description: "Log in to your Tripway Holidays account and manage your travel bookings, explore personalized holiday packages, and enjoy seamless experiences.",
+        url: `${apiPoint}auth/client-login`,
+        type: "website",
+        images: [
+            {
+                url: "/favicon-512.png",
+                width: 400,
+                height: 400,
+                alt: "Login to Tripway Holidays",
+            },
+        ],
+    },
+};
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!email || !password) {
-            setError("Please fill in all fields.");
-            return;
-        }
-
-        try {
-            const personExists = await loginUser(email, password);
-            if (!personExists) {
-                setError("No user found with this email.");
-                return;
-            }
-
-            router.push("/");
-        } catch (error) {
-            setError(error.message); // Display the error message
-            console.error("Error during sign-in:", error);
-        }
-    };
-
-    const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
-
-    return (
-        <>
-            {agentLogin ?
-                <AgentLogin
-                    setAgentLogin={setAgentLogin}
-                    setActiveContainer={setActiveContainer}
-                    activeContainer={activeContainer}
-                />
-                : <div className={styles.loginPage}>
-                    <div className={styles.loginBlur}>
-                        <div className={`${styles.loginContainer} ${activeContainer ? styles.activeCon : styles.closeCon}`}>
-                            <div className={styles.loginCard}>
-                                <Link
-                                    className={styles.backToWeb}
-                                    href="/"
-                                    onMouseEnter={() => setIsHovered(true)}
-                                    onMouseLeave={() => setIsHovered(false)}
-                                >
-                                    <FaHome />
-                                    &nbsp;
-                                    {isHovered && <span className={styles.tooltipText}>Home</span>}
-                                </Link>
-                                <h2 className={styles.loginTitle}>Login</h2>
-                                <form onSubmit={handleSubmit}>
-                                    <div className={styles.formGroup}>
-                                        <label htmlFor="email">Email</label>
-                                        <input
-                                            className={styles.authInput}
-                                            type="email"
-                                            id="client-email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            placeholder="Enter your email"
-                                            required
-                                        />
-                                    </div>
-                                    <div className={styles.formGroup}>
-                                        <label htmlFor="password">Password</label>
-                                        <div className={styles.inputIcon}>
-                                            <input
-                                                className={styles.authInput}
-                                                type={showPassword ? "text" : "password"}
-                                                id="password"
-                                                value={password}
-                                                onChange={(e) => setPassword(e.target.value)}
-                                                placeholder="Enter your password"
-                                                required
-                                            />
-                                            <button
-                                                type="button"
-                                                className={styles.passwordToggleBtn}
-                                                onClick={togglePasswordVisibility}
-                                            >
-                                                {showPassword ? (
-                                                    <MdOutlineVisibilityOff />
-                                                ) : (
-                                                    <MdOutlineVisibility />
-                                                )}
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <p
-                                        className={styles.forgetPassword}
-                                        onClick={() => setActiveContainer(false)}
-                                    >
-                                        Forget Password
-                                    </p>
-                                    {error && <p className={styles.errorMessage}>{error}</p>}
-                                    <button type="submit" className={styles.loginButton}>
-                                        Sign In
-                                    </button>
-                                </form>
-                                <p className={styles.signupLink}>
-                                    Don’t have an account? <a href="/auth/signup">Sign Up</a>
-                                </p>
-                                <a
-                                    href="/auth/agent-login"
-                                    className={styles.forgetPassword}
-                                    onClick={() => setAgentLogin(true)}
-                                >
-                                    Patner Login
-                                </a>
-                            </div>
-                        </div>
-                        <ForgetPassword
-                            activeContainer={activeContainer}
-                            setActiveContainer={setActiveContainer}
-                        />
-                    </div>
-                </div>
-            }
-        </>
-    );
+export default function Page() {
+    return <ClientLoaginPage />
 }
-
-export default ClientLoaginPage;
---
+++ src/app/auth/signup/signup.js
@@ -0,0 +1,182 @@
+'use client';
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaHome } from "react-icons/fa";
+import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
+import { useClient } from "@/context/UserContext";
+import styles from "@/styles/pages/authpage.module.css";
+
+function SignUpPage() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [address, setAddress] = useState("");
+    const [password, setPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [showVerifyPassword, setShowVerifyPassword] = useState(false);
+    const [error, setError] = useState("");
+
+    const { signupUserWithEmailAndPassword } = useClient();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        if (!email || !password || !name || !phoneNumber) {
+            setError("Please fill in all fields.");
+            return;
+        }
+
+        if (password !== verifyPassword) {
+            setError("Passwords do not match.");
+            return;
+        }
+
+        try {
+            await signupUserWithEmailAndPassword(email, password, { name, phoneNumber, verifyPassword, address }, 'users');
+        } catch (err) {
+            setError("Failed to sign up. Please try again.");
+            console.error(err);
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleVerifyPasswordVisibility = () => {
+        setShowVerifyPassword(!showVerifyPassword);
+    };
+
+    return (
+        <div className={styles.signupCustomer}>
+            <div className={styles.loginBlur}>
+                <div className={styles.loginContainer}>
+                    <div className={styles.loginCard}>
+                        <Link
+                            className={styles.backToWeb}
+                            href="/"
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                        >
+                            <FaHome />&nbsp;
+                            {isHovered && <span className={styles.tooltipText}>Home</span>}
+                        </Link>
+                        <h2 className={styles.loginTitle}>Sign Up</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="name">Name</label>
+                                <input
+                                    className={styles.authInput}
+                                    type="text"
+                                    id="name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Enter your Name"
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    className={styles.authInput}
+                                    type="text"
+                                    inputMode="numeric"
+                                    pattern="[0-9]+"
+                                    id="phoneNumber"
+                                    value={phoneNumber}
+                                    onChange={(e) => setPhoneNumber(e.target.value)}
+                                    placeholder="Enter your Phone Number"
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    className={styles.authInput}
+                                    type="email"
+                                    id="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Enter your email"
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="address">Address</label>
+                                <input
+                                    className={styles.authInput}
+                                    type="text"
+                                    id="address"
+                                    value={address}
+                                    onChange={(e) => setAddress(e.target.value)}
+                                    placeholder="Enter your Address"
+                                    required
+                                />
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="password">Password</label>
+                                <div className={styles.inputIcon}>
+                                    <input
+                                        className={styles.authInput}
+                                        type={showPassword ? "text" : "password"}
+                                        id="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        placeholder="Enter your password"
+                                        required
+                                    />
+                                    <button
+                                        type="button"
+                                        className={styles.passwordToggleBtn}
+                                        onClick={togglePasswordVisibility}
+                                    >
+                                        {showPassword ? (
+                                            <MdOutlineVisibilityOff />
+                                        ) : (
+                                            <MdOutlineVisibility />
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="verifyPassword">Verify Password</label>
+                                <div className={styles.inputIcon}>
+                                    <input
+                                        className={styles.authInput}
+                                        type={showVerifyPassword ? "text" : "password"}
+                                        id="verifyPassword"
+                                        value={verifyPassword}
+                                        onChange={(e) => setVerifyPassword(e.target.value)}
+                                        placeholder="Enter your password again"
+                                        required
+                                    />
+                                    <button
+                                        type="button"
+                                        className={styles.passwordToggleBtn}
+                                        onClick={toggleVerifyPasswordVisibility}
+                                    >
+                                        {showVerifyPassword ? (
+                                            <MdOutlineVisibilityOff />
+                                        ) : (
+                                            <MdOutlineVisibility />
+                                        )}
+                                    </button>
+                                </div>
+                            </div>
+                            {error && <p className={styles.errorMessage}>{error}</p>}
+                            <button type="submit" className={styles.loginButton}>
+                                Sign Up
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignUpPage;
