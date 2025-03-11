7533cbb3f7bf4d2fc9a3c019ce1ad883b8ebae08
Ankit Saini <as.ankitsaini406@gmail.com>
2025-03-11 11:43:45 +0530

    changes data for and component

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,16 @@
 import React from "react";
-import Link from "next/link";
 import dynamic from "next/dynamic";
-import Image from "next/image";
-const Video = dynamic(() => import("@/components/slider/video"));
 const TypeWriterLoop = dynamic(() => import("@/utils/typeWriter"));
 const Testimonials = dynamic(() => import("@/components/testimonials"));
 const DelayedComponent = dynamic(() => import("@/utils/DelayedComponent"));
 import { TourSection, WhyBookUs } from "@/components/homeComponents";
 import styles from "./page.module.css";
-import AnimatedHero from "@/components/slider/AnimatedHero";
 import ScrollableCards from "@/components/MotionDiv";
-// import PopUp from "@/utils/popUp";
 
 export const metadata = {
-    title: "TripWay Holidays: Book One-way | Group Tour | Multi City",
+    title: "TripWay Holidays: Book One-way | Round Trip | Multi City | Group Tour",
     description: "Embark on a journey like never before with TripWay Holidays, your ultimate travel partner. Whether you're planning a one-way trip, a round-trip adventure, a multi-city exploration, or a memorable group tour, we have you covered. Discover the best tour packages tailored to your preferences. Experience seamless booking, personalized itineraries, and unmatched customer service. From picturesque landscapes to bustling cities, TripWay Holidays ensures that every journey is unforgettable. Book now and turn your travel dreams into reality.",
 };
-
 
 const Home = () => {
 
@@ -41,14 +35,16 @@ const Home = () => {
             description: "For those planning multi-city trips Tripway Holidays offers reliable and flexible services. You can hire a single vehicle for multiple destinations avoiding multiple bookings. Our fleet includes luxury SUVs and economy vehicles. With experienced drivers and well-maintained cars we guarantee safety, on-time arrivals and a hassle-free travel experience tailored to your itinerary and group size.",
             imageSrc: "/slider/slider2.webp",
         },
+        {
+            id: "groupTour",
+            title: "Group Tour",
+            description: "Tripway Holidays offers group tours blending spirituality, culture, and exploration for unforgettable experiences. Embark on spiritual journeys fostering tranquility and connections, or dive into heritage tours exploring customs, art, and history. Enjoy Freedom Tours for flexibility and companionship, or celebrate vibrant festivals with Special Event Tours. Every trip combines joy, connection, and discovery, tailored to your interests and mood.",
+            imageSrc: "/slider/slider6.webp",
+        },
     ];
 
     return (
         <>
-        {/* <PopUp popTime={10000} closeTime={15000} src="/popup/lucky-image.webp" alt="Lucky Draw Image" /> */}
-            {/* <Hero /> */}
-            {/* <AnimatedHero /> */}
-            {/* <Video /> */}
             <ScrollableCards />
 
             <div className={styles.hadingBox}>
@@ -70,31 +66,6 @@ const Home = () => {
                     />
                 ))}
 
-                <div className={styles.homeTour}>
-                    <div className={styles.homeTourFlex}>
-                        <div className={styles.scrollRevel}>
-                            <h2> <span>Group Tours</span></h2>
-                            <p><span>
-                                Tripway Holidays offers group tours blending spirituality, culture, and exploration for unforgettable experiences. Embark on spiritual journeys fostering tranquility and connections, or dive into heritage tours exploring customs, art, and history. Enjoy Freedom Tours for flexibility and companionship, or celebrate vibrant festivals with Special Event Tours. Every trip combines joy, connection, and discovery, tailored to your interests and mood.
-                            </span>
-                            </p>
-                            {/* <Link className='readMore' href='/group-tour'>Read More</Link> */}
-                        </div>
-                        <div className={styles.imgBox}>
-                            <Image
-                                sizes="(max-width: 768px) 100vw, 50vw"
-                                className={styles.tourImg}
-                                data-src={`/slider/slider6.webp`}
-                                src={`/slider/slider6.webp`}
-                                alt="multicolored-buntings"
-                                placeholder="blur"
-                                blurDataURL={`/slider/slider6.webp`}
-                                loading="lazy"
-                                fill
-                            />
-                        </div>
-                    </div>
-                </div>
                 <DelayedComponent delay={1000} >
                     <WhyBookUs />
                     <Testimonials category='home' />
