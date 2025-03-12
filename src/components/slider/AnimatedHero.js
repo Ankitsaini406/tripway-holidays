04d4da4ec44bdcb4882eda1bdbadcdadbdfc8d1f
Ankit Saini <as.ankitsaini406@gmail.com>
2025-03-12 14:58:50 +0530

    add button in hero section

--- src/components/slider/AnimatedHero.js
+++ src/components/slider/AnimatedHero.js
@@ -1,4 +1,5 @@
 import styles from "@/styles/components/hero.module.css";
+import Link from "next/link";
 
 export default function AnimatedHero() {
     return (
@@ -6,6 +7,13 @@ export default function AnimatedHero() {
             <div className={styles.heroContent}>
                 <h1>&quot;Your - Trusted Travel Partner&quot;</h1>
                 <p>Connecting Destinations, Creating Memories!</p>
+            </div>
+
+            <div className={styles.allButton}>
+                <Link className={styles.linksButton} href="/cabs/one-way">One Way</Link>
+                <Link className={styles.linksButton} href="/cabs/round-trip">Round Trip</Link>
+                <Link className={styles.linksButton} href="/cabs/multi-city">Multi City</Link>
+                <Link className={styles.linksButton} href="/group-tour">Group Tours</Link>
             </div>
         </div>
     );
