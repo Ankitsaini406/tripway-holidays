9de9971b0062742aa1e81692e98b21f75e489994
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-01 16:15:07 +0530

    height of not found change and footer change

--- src/app/childLayout.js
+++ src/app/childLayout.js
@@ -1,6 +1,5 @@
 'use client';
 
-import "./globals.css";
 import { useEffect } from "react";
 import dynamic from "next/dynamic";
 import Header from "../components/header";
@@ -8,7 +7,7 @@ import { usePathname } from "next/navigation";
 import { ToastContainer } from "react-toastify";
 const Footer = dynamic(() => import("@/components/footer"));
 import "react-toastify/dist/ReactToastify.css";
-import DelayedComponent from "@/utils/DelayedComponent";
+import "./globals.css";
 
 export default function ClientRootLayout({ children }) {
     const pathname = usePathname();
@@ -40,11 +39,7 @@ export default function ClientRootLayout({ children }) {
             {!hideHeaderFooter && <Header />}
             <main className="minHeight">{children}</main>
             <ToastContainer position="top-right" autoClose={10000} hideProgressBar={false} draggable={true} closeOnClick />
-            {!hideHeaderFooter &&
-                <DelayedComponent delay={1000} >
-                    <Footer />
-                </DelayedComponent>
-            }
+            {!hideHeaderFooter && <Footer />}
         </>
     );
 }
