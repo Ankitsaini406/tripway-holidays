bf20da065dede85285ec5b8a4f6712abf58c074d
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-12 11:58:08 +0530

    add titile and meta data in driver auth

--- src/app/auth/driver/login/page.js
+++ src/app/auth/driver/login/page.js
@@ -1,5 +1,40 @@
 import DriverLoginPage from "./Driverlogin";
 
+export const metadata = {
+    title: "Driver Login",
+    description: "Log in to your Tripway Holidays agent account. Manage bookings, access client details, and explore exclusive resources designed for agents.",
+    keywords: [
+        "Driver Login Tripway Holidays",
+        "Driver Portal Login",
+        "Tripway Holidays Driver Account",
+        "Login for Drivers",
+        "Manage Bookings Driver Portal",
+        "Driver Sign In Tripway",
+        "Tripway Holidays Driver Login Page",
+    ],
+    openGraph: {
+        title: "Driver Login",
+        description: "Access your Tripway Holidays agent account. Log in to manage client bookings, view travel packages, and leverage tools to enhance your services.",
+        url: "https://tripwayholidays.in/auth/driver/login",
+        type: "website",
+        images: [
+            {
+                url: "/favicon-512.png",
+                width: 400,
+                height: 400,
+                alt: "Driver Login to Tripway Holidays",
+            },
+        ],
+    },
+    meta: [
+        { name: "robots", content: "index, follow" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { name: "author", content: "Tripway Holidays - Driver Login" },
+        { name: "theme-color", content: "#ffffff" },
+        { charset: "UTF-8" },
+    ],
+};
+
 export default async function Page() {
     return <DriverLoginPage />
 }
--- src/app/auth/driver/signup/page.js
+++ src/app/auth/driver/signup/page.js
@@ -1,5 +1,39 @@
 import DriverSignup from "./DriverSignup";
 
+export const metadata = {
+    title: "Driver Sign In",
+    description: "Create your account with Tripway Holidays. Driver Sign up today to explore personalized holiday packages, exclusive deals, and seamless bookings.",
+    keywords: [
+        "Driver Sign Up Tripway Holidays",
+        "Driver Register Tripway Account",
+        "Holiday Package Registration",
+        "Create Driver Account Tripway Holidays",
+        "Driver Sign Up for Trip Packages",
+        "Tripway Holidays Driver Sign-Up Form",
+        "Exclusive Holiday Deals Driver Sign-Up",
+    ],
+    openGraph: {
+        title: "Driver Sign Up",
+        description: "Join Tripway Holidays to unlock amazing travel experiences, curated holiday packages, and exclusive offers. Driver Sign up and get started on your journey today!",
+        url: "https://tripwayholidays.in/auth/dirver/signup",
+        type: "website",
+        images: [
+            {
+                url: "/favicon-512.png",
+                width: 400,
+                height: 400,
+                alt: "Driver Sign Up for Tripway Holidays",
+            },
+        ],
+    },
+    meta: [
+        { name: "robots", content: "index, follow" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { name: "author", content: "Tripway Holidays - Driver Sign Up" },
+        { name: "theme-color", content: "#ffffff" },
+        { charset: "UTF-8" },
+    ],
+};
 
 export default async function Page() {
     return <DriverSignup />;
