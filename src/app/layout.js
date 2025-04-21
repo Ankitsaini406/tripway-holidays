9c6969b864cae7855588b3c8369761dce0fcdfe1
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-21 09:50:28 +0530

    changes for icons and image for favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -48,12 +48,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         {/* Favicon and Icons */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="preload" as="image" href="/favicon-512.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-192.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
 
         {/* Mobile Web App Settings */}
         <meta name="mobile-web-app-capable" content="yes" />
