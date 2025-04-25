b97fa57f23b80bca5f7a60ce0d018d27f92f1375
Ankit Saini <as.ankitsaini406@gmail.com>
2025-04-26 00:24:54 +0530

    stiemap push

--- src/app/api/robots/route.js
+++ src/app/api/robots/route.js
@@ -1,17 +1,16 @@
 
 export async function GET() {
 
-    const localApi = process.env.API_URL;
-    const productionApi = process.env.HOST_URL;
-    const apiPoint = process.env.NODE_ENV === "development" ? localApi : productionApi;
+    // const localApi = process.env.API_URL;
+    // const productionApi = process.env.HOST_URL;
+    // const apiPoint = process.env.NODE_ENV === "development" ? localApi : productionApi;
 
     const robots = `
         User-agent: *
         Allow: /
         Disallow: /admin/
+        Sitemap: https://tripwayholidays.in/sitemap.xml
     `;
-
-    // Sitemap: ${apiPoint}sitemap.xml
 
     return new Response(robots.trim(), {
         headers: {
